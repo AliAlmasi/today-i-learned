908e889bbf9ec9bd655274804929268faeaff77c
Ali Almasi <a710almasi@gmail.com>
2024-11-17 15:38:56 +0330

    Imported Supabase API key via another way

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import supabaseKey from "./apikey";
+
 const supabaseUrl = "https://inrebhkeimbgngqlqvgf.supabase.co";
-const supabaseKey = process.env.supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
