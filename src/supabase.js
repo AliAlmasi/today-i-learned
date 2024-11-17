84db99d438615b199c84326f22b3193992cae416
Ali Almasi <a710almasi@gmail.com>
2024-11-17 13:59:20 +0330

    updated env and supabase api key

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://inrebhkeimbgngqlqvgf.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmViaGtlaW1iZ25ncWxxdmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MTg3NTcsImV4cCI6MjA0NzA5NDc1N30.TXiJOnEuLp4fCGR7vrWz4hF_E0CdPXtQWJHh-nSn1aU";
+const supabaseKey = process.env.supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
