1ffa3e8a76740773d4345fe584f50ef66d0ceef0
Ali Almasi <a710almasi@gmail.com>
2024-11-17 15:43:43 +0330

    API KEY ADDED

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import supabaseKey from "./apikey";
-
 const supabaseUrl = "https://inrebhkeimbgngqlqvgf.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmViaGtlaW1iZ25ncWxxdmdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MTg3NTcsImV4cCI6MjA0NzA5NDc1N30.TXiJOnEuLp4fCGR7vrWz4hF_E0CdPXtQWJHh-nSn1aU";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
